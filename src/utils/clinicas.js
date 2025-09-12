0657c135b300536bbccdf3e273ba27407623262b
FabriAV <acostafabricio2007@gmail.com>
2025-09-12 10:27:25 -0500

    added sections and functionalities

---
+++ src/utils/clinicas.js
@@ -0,0 +1,92 @@
+export const clinicas = [
+    {
+        name: "Hospital General de Jaén",
+        x: -5.705373325191959,
+        y: -78.80464442772474
+    },
+    {
+        name: "Clínica de la Solidaridad",
+        x: -5.706174198849131,
+        y: -78.80616440200674
+    },
+    {
+        name: "Clínica San Juan",
+        x: -5.712540048774474,
+        y: -78.80985017258813
+    },
+    {
+        name: "Centro Médico San Nicolas",
+        x: -5.71322902703546,
+        y: -78.80409860285822
+    },
+    {
+        name: "Clínica Quiruclinic",
+        x: -5.709698556274119,
+        y: -78.80921477373782
+    },
+    {
+        name: "Neuromedic Jaén",
+        x: -5.7079863429839195,
+        y: -78.80374446325663
+    },
+    {
+        name: "Cadilab",
+        x: -5.704620688040476,
+        y: -78.8038209586032
+    },
+    {
+        name: "Cadilab",
+        x: -5.704067555769821,
+        y: -78.80346599023848
+    },
+    {
+        name: "Lab Norberth Winnner Jaén",
+        x: -5.704267627131217,
+        y: -78.80350903078251
+    },
+    {
+        name: "Clínica Ebenezer",
+        x: -5.706298721699746,
+        y: -78.80628060471976
+    },
+    {
+        name: "Clínica Belen",
+        x: -5.706535049217318,
+        y: -78.80615528024403
+    },
+    {
+        name: "Clínica Ángel",
+        x: -5.705836714687811,
+        y: -78.80819438797508
+    },
+    {
+        name: "Clínica Alienta",
+        x: -5.704060206798431,
+        y: -78.804055029662
+    },
+    {
+        name: "Hospital De Apoyo Bagua Gustavo Lanatta Luján",
+        x: -5.638942865347323,
+        y: -78.53025231428767
+    },
+    {
+        name: "Clínica Interamericana",
+        x: -5.633076917450354,
+        y: -78.53562549653034
+    },
+    {
+        name: "Clínica Pediatric Kids y Especialidades",
+        x: -5.6393252203542055,
+        y: -78.53026569810483
+    },
+    {
+        name: "Clínica Montecarlo",
+        x: -5.639532086270268,
+        y: -78.53119115722639
+    },
+    {
+        name: "Cadilab",
+        x: -5.6384977070841025,
+        y: -78.52955952708567
+    },
+]
