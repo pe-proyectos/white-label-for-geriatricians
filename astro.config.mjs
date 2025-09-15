04133a890a198545a8769a62efb3716b32514a6c
ChoqueCastroLD <luis.choque.castro@outlook.com>
2025-09-15 09:25:32 -0500

    feat: updated dockerfile for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
+import bun from 'astro-bun-adapter';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    server: {
+      host: true,
+      port: 4322,
+    },
+    prefetch: false,
+    adapter: bun(),
+    integrations: [
+        tailwind(),
+    ],
+    devToolbar: {
+        enabled: false,
+    },
+});
